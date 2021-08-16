c724ec5536c772d81c5bc0e0800bbf5bea1694f4
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-16 19:40:59 +0900

    first

---
+++ php1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  echo "みなさん", "おはようございます <br>", PHP_EOL;
+  print "お目覚めは". "いかかですか？ <br><br>" . PHP_EOL;
+  echo "小鳥のさえずりが聞こえます <br>", PHP_EOL;
+  echo "小鳥のさえずりが聞こえます <br>", PHP_EOL;
+?>
+</body>
+</html>
