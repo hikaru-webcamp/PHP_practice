dda3ca52f8b8076793ed4edec936795e1216ff03
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-19 14:51:41 +0900

    配列追加練習

--- php4.php
+++ php4.php
@@ -30,3 +30,25 @@ echo $days["afternoon"],"になります。<br>";
 echo $days["night"],"になります。<br>";
 
 ?>
+
+<?php
+  $arr1[] = "123";
+  $arr1[] = "季節";
+  $arr1[] = 456.789;
+
+$arr2["spring"] = "春";
+$arr2["summer"] = "夏";
+$arr2["autumn"] = "秋";
+$arr2["winter"] = "冬";
+
+echo $arr1[0], "<br>";
+echo $arr1[1], "<br>";
+echo $arr1[2], "<br>";
+
+echo "<br>",PHP_EOL;
+
+echo $arr2["spring"], "<br>";
+echo $arr2["summer"], "<br>";
+echo $arr2["autumn"], "<br>";
+echo $arr2["winter"], "<br>";
+?>
